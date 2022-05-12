26bf2a376a05e6f34ebf2a13cf2a2b7e41277927
Maximalfocus <21332201+zhubaiyuan@users.noreply.github.com>
2022-05-12 13:24:11 +0800

    sudoku solver

--- challenges/c075_sudoku_solver_v2.py
+++ challenges/c075_sudoku_solver_v2.py
@@ -19,8 +19,8 @@ def _solve_sudoku_helper(board, start_row, start_col):
                 return True
     solved = False
     for num in range(1, 10):
-        board[row][col] = num
-        if is_valid_position(board):
+        if is_valid_position(board, row, col, num):
+            board[row][col] = num
             if col < 8:
                 solved = _solve_sudoku_helper(board, row, col + 1)
             else:
@@ -34,66 +34,51 @@ def _solve_sudoku_helper(board, start_row, start_col):
     return False
 
 
-def is_valid_position(board):
-    return check_horizontally(board) and \
-        check_vertically(board) and \
-        check_boxes(board)
+def is_valid_position(board, row, col, num):
+    return check_num_not_in_column(board, col, num) and \
+        check_num_not_in_row(board, row, num) and \
+        check_num_not_in_box(board, row, col, num)
 
 
-def check_horizontally(board):
+def check_num_not_in_column(board, col, num):
     for row in range(9):
-        row_values = [board[row][x] for x in range(9)]
-        if not all_desired_numbers(row_values):
+        if board[row][col] == num:
             return False
     return True
 
 
-def check_vertically(board):
-    for x in range(9):
-        column_values = [board[row][x] for row in range(9)]
-        if not all_desired_numbers(column_values):
+def check_num_not_in_row(board, row, num):
+    for col in range(9):
+        if board[row][col] == num:
             return False
     return True
 
 
-def check_boxes(board):
-    for y_box in range(3):
-        for x_box in range(3):
-            box_values = collect_box_values(board, y_box, x_box)
-            if not all_desired_numbers(box_values):
+def check_num_not_in_box(board, row, col, num):
+    adjusted_row = row // 3 * 3
+    adjusted_col = col // 3 * 3
+    for y in range(3):
+        for x in range(3):
+            if board[adjusted_row + y][adjusted_col + x] == num:
                 return False
     return True
 
 
-def collect_box_values(board, y_box, x_box):
-    box_values = []
-    for y in range(3):
-        for x in range(3):
-            real_y = y_box * 3 + y
-            real_x = x_box * 3 + x
-            box_values.append(board[real_y][real_x])
-    return box_values
-
-
 def all_desired_numbers(all_collected_values):
     relevant_values = list(all_collected_values)
-    relevant_values = remove_all_occurences(relevant_values, 0)
+    remove_all_occurences(relevant_values, 0)
     values_set = set(relevant_values)
     if len(relevant_values) != len(values_set):
         return False
     return {1, 2, 3, 4, 5, 6, 7, 8, 9}.issuperset(values_set)
 
 
-def remove_all_occurences(values, val):
-    return [value for value in values if value != val]
-
-
-def is_sudoku_valid(board):
-    return check_vertically(board) and check_horizontally(board) and check_boxes(board)
+def remove_all_occurences(the_list, val):
+    return [value for value in the_list if value != val]
 
 
 def main():
-    board = [
+    board1 = [
         [1, 2, 0, 4, 5, 0, 7, 8, 9],
         [0, 5, 6, 7, 0, 9, 0, 2, 3],
         [7, 8, 0, 1, 2, 3, 4, 5, 6],
@@ -103,14 +88,9 @@ def main():
         [5, 3, 1, 6, 0, 2, 9, 0, 8],
         [6, 0, 2, 9, 7, 8, 5, 3, 1],
         [9, 7, 0, 0, 3, 1, 6, 4, 2]]
-    print(board)
-    print("V: ", check_vertically(board))
-    print("H: ", check_horizontally(board))
-    print("B: ", check_boxes(board))
-    print("S: ", is_sudoku_valid(board))
-    if solve_sudoku(board):
+    if solve_sudoku(board1):
         print("Solved!")
-    print_array(board)
+    print_array(board1)
     print("-------------------------")
     board2 = [
         [6, 0, 2, 0, 5, 0, 0, 0, 0],
--- challenges/c075_sudoku_solver_v3.py
+++ challenges/c075_sudoku_solver_v3.py
@@ -20,12 +20,12 @@ def _solve_sudoku_helper(board, start_row, start_col):
     solved = False
     for num in range(1, 10):
         board[row][col] = num
-        if is_valid_position(board):
-            if col < 8:
+        if is_valid_position(board, row, col):
+            if (col < 8):
                 solved = _solve_sudoku_helper(board, row, col + 1)
             else:
                 solved = _solve_sudoku_helper(board, row + 1, 0)
-            if not solved:
+            if (not solved):
                 board[row][col] = 0
             else:
                 return True
@@ -34,35 +34,25 @@ def _solve_sudoku_helper(board, start_row, start_col):
     return False
 
 
-def is_valid_position(board):
-    return check_horizontally(board) and \
-        check_vertically(board) and \
-        check_boxes(board)
+def is_valid_position(board, row, col):
+    return check_single_horizontally(board, row) and \
+        check_single_vertically(board, col) and \
+        check_single_box(board, row, col)
 
 
-def check_horizontally(board):
-    for row in range(9):
-        row_values = [board[row][x] for x in range(9)]
-        if not all_desired_numbers(row_values):
-            return False
-    return True
+def check_single_horizontally(board, row):
+    column_values = [board[row][col] for col in range(9)]
+    return all_desired_numbers(column_values)
 
 
-def check_vertically(board):
-    for x in range(9):
-        column_values = [board[row][x] for row in range(9)]
-        if not all_desired_numbers(column_values):
-            return False
-    return True
+def check_single_vertically(board, col):
+    row_values = [board[row][col] for row in range(9)]
+    return all_desired_numbers(row_values)
 
 
-def check_boxes(board):
-    for y_box in range(3):
-        for x_box in range(3):
-            box_values = collect_box_values(board, y_box, x_box)
-            if not all_desired_numbers(box_values):
-                return False
-    return True
+def check_single_box(board, row, col):
+    box_values = collect_box_values(board, row // 3, col // 3)
+    return all_desired_numbers(box_values)
 
 
 def collect_box_values(board, y_box, x_box):
@@ -84,16 +74,12 @@ def all_desired_numbers(all_collected_values):
     return {1, 2, 3, 4, 5, 6, 7, 8, 9}.issuperset(values_set)
 
 
-def remove_all_occurences(values, val):
-    return [value for value in values if value != val]
-
-
-def is_sudoku_valid(board):
-    return check_vertically(board) and check_horizontally(board) and check_boxes(board)
+def remove_all_occurences(the_list, val):
+    return [value for value in the_list if value != val]
 
 
 def main():
-    board = [
+    board1 = [
         [1, 2, 0, 4, 5, 0, 7, 8, 9],
         [0, 5, 6, 7, 0, 9, 0, 2, 3],
         [7, 8, 0, 1, 2, 3, 4, 5, 6],
@@ -103,14 +89,9 @@ def main():
         [5, 3, 1, 6, 0, 2, 9, 0, 8],
         [6, 0, 2, 9, 7, 8, 5, 3, 1],
         [9, 7, 0, 0, 3, 1, 6, 4, 2]]
-    print(board)
-    print("V: ", check_vertically(board))
-    print("H: ", check_horizontally(board))
-    print("B: ", check_boxes(board))
-    print("S: ", is_sudoku_valid(board))
-    if solve_sudoku(board):
+    if solve_sudoku(board1):
         print("Solved!")
-    print_array(board)
+    print_array(board1)
     print("-------------------------")
     board2 = [
         [6, 0, 2, 0, 5, 0, 0, 0, 0],
