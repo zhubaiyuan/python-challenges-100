c436d3b5918fa6debda04fca3d1468c10ce39e69
Maximalfocus <21332201+zhubaiyuan@users.noreply.github.com>
2022-05-13 17:22:51 +0800

    insertion sort

---
+++ tests/test_c096_insertion_sort.py
@@ -0,0 +1,7 @@
+from challenges.c096_insertion_sort import insertion_sort
+
+
+def test_insertion_sort():
+    values = [7, 2, 5, 1, 6, 8, 9, 4, 3]
+    insertion_sort(values)
+    assert values == [1, 2, 3, 4, 5, 6, 7, 8, 9]
